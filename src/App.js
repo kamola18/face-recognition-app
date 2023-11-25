9f41458bf54c6a2ab0e25f8bbf0fd43806274837
kamola18 <orifovakamola17@gmail.com>
2023-11-25 22:39:31 +0100

    render linked

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3001/imageurl', {
+      fetch('https://face-recognition-app-xmd6.onrender.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -84,7 +84,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3001/image', {
+          fetch('https://face-recognition-app-xmd6.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
