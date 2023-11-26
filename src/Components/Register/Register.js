94563b777ca951bbf338a0066758c71d1050b4ab
kamola18 <orifovakamola17@gmail.com>
2023-11-26 09:57:47 +0100

    backend

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
 
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3001/register',{
+        fetch('https://faceapp-backend.onrender.com/register',{
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
